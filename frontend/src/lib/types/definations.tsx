a284f2e0ac0ad75d94b13e8846671da19d2eccad
Kürşat Polatcı <polatcikursat58@gmail.com>
2025-05-27 12:01:15 +0300

    Server

--- frontend/src/lib/types/definations.tsx
+++ frontend/src/lib/types/definations.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API_URL =
-  import.meta.env.MODE === 'development' ? `http://localhost:5000/api` : `api`;
+  import.meta.env.MODE === 'development' ? `http://localhost:5000/api` : `https://server.kursatpolatci.com`;
 export type dialogType = 'edit' | 'add';
 
 export const axiosInstance = axios.create({
